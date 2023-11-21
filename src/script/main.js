8c8aeae3949a3131aa4fa63eda43711b89012a85
Etheriouse <Etheriouse.7@gmail.com>
2023-11-21 01:54:52 +0100

    Rework
    rework of yuzuko

--- src/script/main.js
+++ src/script/main.js
@@ -1,4 +1,4 @@
-let inp = document.getElementById("inputtxt");
+let inp = document.getElementById("entrertext");
 
 inp.addEventListener("keyup", (e) => {
   if (e.key == "Enter") {
@@ -6,120 +6,111 @@ inp.addEventListener("keyup", (e) => {
     let val = inp.value;
     console.log(val);
     inp.value = "";
-    if(val != "") {
-      addMessage("Etheriouse", val, "Yuzu_thinks.png")
+    if (val != "") {
+      addMsg("pseudo", val, "Yuzu_thinks.png");
     }
   }
 });
-function Message() {
-  alert("Go to Message");
+
+let addsearch = document.getElementById("addorsearchimg");
+
+let add = false;
+
+function shiftispressed() {
+  return add;
 }
 
-function General() {
-  alert("Go to General");
+document.addEventListener("keydown", (e) => {
+  if (e.shiftKey) {
+    add = !add;
+
+    if (add) {
+      addsearch.src = `../asset/add.png`;
+      addsearch.style.width = "40px";
+    }
+  }
+  if (!add) {
+    addsearch.src = `../asset/search.png`;
+  }
+});
+
+function addthread() {
+  if (shiftispressed()) {
+    let name = document.getElementById("soa");
+    console.log(name.value);
+    console.log(name);
+    if (name.value != "") {
+      let threadname = document.createTextNode(name.value);
+      let thread = document.createElement("div");
+      let threadtitle = document.createElement("h3");
+      let linktothread = document.createElement("a");
+
+      linktothread.appendChild(threadname);
+      threadtitle.appendChild(linktothread);
+      thread.appendChild(threadtitle);
+
+      thread.classList.add("threadx");
+      linktothread.classList.add("linktoThread");
+      linktothread.href = `thread/${name.value}.html`;
+
+      let parentDiv = document.getElementById("contentAside");
+      let chidldiv = document.getElementById("threadx");
+
+      console.log(thread);
+
+      console.log(thread.outerHTML);
+
+      parentDiv.insertBefore(thread, chidldiv);
+      parentDiv.insertBefore(thread, chidldiv);
+    
+      name.value = "";
+    }
+  }
 }
 
-function addDive(pseudonime, messageContent, timeSetNow, SrcPdp) {
+function addMsg(pseudonime, messageContent, pathPdp) {
+  let now = new Date();
+  let timeSetNow =
+    now.getFullYear() +
+    "/" +
+    now.getMonth() +
+    "/" +
+    now.getDay() +
+    " " +
+    now.getHours() +
+    ":" +
+    now.getMinutes() +
+    ":" +
+    now.getSeconds();
 
-    let msg1 = document.createElement("div");
-    let namepdptime = document.createElement("div");
-    let pdp = document.createElement("img");
-    let pseudotime = document.createElement("h3");
-    let contents = document.createElement("div");
-    let message = document.createElement("p");
-    let pseudo = document.createElement("span");
-    let time = document.createElement("span");
+  let messagex = document.createElement("div");
+  let headermsg = document.createElement("div");
+  let pdp = document.createElement("img");
+  let pseudo = document.createElement("h4");
+  let contentmsg = document.createElement("p");
 
-    let tpseudo = document.createTextNode(pseudonime);
-    let ttime = document.createTextNode(timeSetNow);
+  let tpseudo = document.createTextNode(`${pseudonime} ${timeSetNow}`);
 
-    let msg = document.createTextNode(messageContent);
+  let msg = document.createTextNode(messageContent);
 
-    msg1.classList.add("msg1");
-    msg1.setAttribute("id", "mess")
-    namepdptime.classList.add("namepdptime");
-    pdp.classList.add("pdp");
-    pseudotime.classList.add("pseudotime");
-    contents.classList.add("contents");
-    pdp.src = `../asset/${SrcPdp}`;
-    pdp.style.maxWidth = "100%";
-    pseudo.classList.add("pseudo");
-    time.classList.add("time");
-    
-    /* msg1.style.display = "flex";
-    msg1.style.flexDirection = "column";
-    msg1.style.margin = "10px";
-    msg1.style.padding = "10px";
-    msg1.style.borderRadius = "5px";
-    msg1.style.border = "solid black 5px";
-    msg1.style.backgroundColor = "red";
+  messagex.classList.add("messagex");
+  messagex.setAttribute("id", "messagex");
+  headermsg.classList.add("headermsg");
+  pdp.classList.add("pdp");
+  pseudo.classList.add("pseudo");
+  pdp.src = `../asset/${pathPdp}`;
+  pseudo.classList.add("pseudo");
+  contentmsg.classList.add("contentmsg");
 
-    pdp.style.maxWidth = "100%";
-    pdp.style.maxHeight = "100%";
-    pdp.style.borderRadius = "50%";
+  pseudo.appendChild(tpseudo);
+  contentmsg.appendChild(msg);
+  headermsg.appendChild(pdp);
+  headermsg.appendChild(pseudo);
+  messagex.appendChild(headermsg);
+  messagex.appendChild(contentmsg);
 
-    namepdptime.style.display = "flex";
-    namepdptime.style.flexDirection = "row";
-    namepdptime.style.height = "70px";
-    namepdptime.style.width = "100px";
+  let parentDiv = document.getElementById("spacetalk");
+  let chidldiv = document.getElementById("messagex");
 
-    pseudotime.style.display = "flex";
-    pseudotime.style.flexDirection = "row";
-
-    pseudo.style.fontSize = "x-large";
-    pseudo.style.fontFamily = "sans-serif";
-    pseudo.style.fontWeight = "600";
-    pseudo.style.paddingLeft = "5%";
-
-    time.style.fontSize = "x-large";
-    time.style.fontFamily = "sans-serif";
-    time.style.fontWeight = "100";
-    time.style.paddingLeft = "5%";
-    time.style.fontStyle = "italic";
-
-    contents.style.fontSize = "large";
-    contents.style.fontFamily = "sans-serif";
-    contents.style.fontWeight = "500";
-    contents.style.paddingLeft = "6%";*/
-
-    pseudo.appendChild(tpseudo);
-    time.appendChild(ttime);
-    pseudotime.appendChild(pseudo);
-    pseudotime.appendChild(time);
-    message.appendChild(msg);
-    namepdptime.appendChild(pdp);
-    namepdptime.appendChild(pseudotime);
-    contents.appendChild(message);
-    msg1.appendChild(namepdptime);
-    msg1.appendChild(contents);
-
-    let parentDiv = document.getElementById("tchatSpace");
-    let chidldiv = document.getElementById("mess")
-
-    parentDiv.insertBefore(msg1, chidldiv);
-    //parentDiv.appendChild(msg1);
-    /*
-              let testdiv = document.createElement("div");
-              testdiv.innerHTML(cot);
-
-              let div = document.createElement("div");
-              div.style.backgroundColor = "red";
-              div.style.margin = "auto";
-              div.style.padding = "auto";
-              div.style.width = "100%";
-              div.style.height = "100%"
-              let parentDiv = document.getElementById("conta");
-              parentDiv.appendChild(div);
-  */
-    //parentDiv.innerHTML=div;
-    //div.childNodes.appendChild(div)
-    //document.body.appendChild(div)
-  }
-
-  function addMessage(pseudonime, messageContent, SrcPdp) {
-    
-    let now = new Date();
-    let time = now.getFullYear()+"/"+now.getMonth()+"/"+now.getDay()+" "+now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
-
-    addDive(pseudonime, messageContent, time, SrcPdp)
-  }
+  parentDiv.insertBefore(messagex, chidldiv);
+}
